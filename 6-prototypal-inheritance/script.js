2641f1f0bcdf8697c6be5bdcfc4bbbc070ff5c0c
sakshatl <voteforsakshat@gmail.com>
2023-11-01 21:33:54 +0530

    Prototypal Inheritance using __proto__

--- 6-prototypal-inheritance/script.js
+++ 6-prototypal-inheritance/script.js
@@ -1,4 +1,5 @@
-let parent = {
+// Create an object 'a' using object literal syntax
+let a = {
   name: 'Minato',
   age: 30,
   greet: function() {
@@ -6,8 +7,64 @@ let parent = {
   }
 };
 
-// Create a new 'child' object with 'parent' as its prototype
-let child = Object.create(parent);
+// Create an object 'b' with prototype of 'a'
+let b = Object.create(a);
+b['name'] = "Naruto";
 
-// Accessing methods and properties throught prototype chain
-console.log(child.greet());
+
+// === DRIVER CODE ===========
+
+// 1.
+console.log(a);
+
+/* 
+	- methods and properties of a
+	- [[Prototype]] of 'a' which is methods/properties of global 'Object'
+*/
+
+// 2. 
+console.log(a.__proto__);
+
+/* 
+	- [[Prototype]] of 'a' which is methods/properties of global 'Object'
+*/
+
+// 3.
+console.log(a.__proto__.__proto__); 
+
+/* 
+	- 'null' end of prototype chain
+*/
+
+// 4. 
+console.log(b);
+
+/* 
+	- methods and properties of 'b'
+	- [[Prototype]] of 'b' which is methods/properties of 'a'
+	- [[Protptype]] of 'a' which is Global object
+*/
+
+// 5. 
+console.log(b.__proto__);
+
+/* 
+	- [[Prototype]] of 'b' which is methods/properties of 'b'
+	- [[Protptype]] of 'a' which is Global object
+*/
+
+// 6.
+console.log(b.__proto__.__proto__.__proto__);
+
+/* 
+	- 'null' end of prototype chain 
+*/
+
+// 7.
+console.log(b.greet()); // RASENGAN!!!
+
+/* 
+	- `greet` methods does not exist on object 'b'
+	- JavaScript Engine will look up in the prototype chain 
+	- It will find `greet` method on 'a' 
+*/
